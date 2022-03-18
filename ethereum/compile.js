8c58d8f093eb2b0ebd67f900a1f4590b73675464
Goncalo Silva <gsilva@trilliumsystems.net>
2022-03-18 13:34:10 +0000

    Added build files compiler

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,3 +1,24 @@
 const path = require("path");
 const solc = require("solc");
 const fs = require("fs-extra");
+
+const buildPath = path.resolve(__dirname, "build");
+
+//clear build folder
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+
+//compile both contracts
+const output = solc.compile(source, 1).contracts;
+
+//ensure build folder exists, if not create
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+	fs.outputJSONSync(
+		path.resolve(buildPath, contract.replace(":", "") + ".json"),
+		output[contract]
+	);
+}
